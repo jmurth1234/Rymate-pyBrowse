9607cf26356477e62b613434a6bc61c7d06b866a
rymate1234 <rymate@rymate-Aspire-5551.(none)>
2012-01-29 08:05:32 +0000

    removed back and forward buttons

--- browser.py
+++ browser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import webkit
+import gnome.ui
 try:
  	import pygtk
   	pygtk.require("2.0")
@@ -12,8 +13,12 @@ try:
 except:
 	sys.exit(1)
 	
+APPNAME="Rymate pyBrowse"
+APPVERSION="0.1"
+	
 class HelloWorldGTK:
 	def __init__(self):
+		gnome.init(APPNAME, APPVERSION)
 		
 		#Set the Glade file
 		self.gladefile = "gui.glade"  
@@ -25,19 +30,19 @@ class HelloWorldGTK:
                 self.view.open("http://google.com")
 		
 		#Sets connections and stuff
-		dic = { "on_btnGo_clicked" : self.btnGo_clicked,
-	        "on_Window_destroy" : gtk.main_quit }
+		dic = { "on_btnGo_clicked" : self.btnGo_clicked }
                 self.wTree.signal_autoconnect(dic)
 		
 		self.go = self.wTree.get_widget("button3")
 		self.window = self.wTree.get_widget("Window")
+		if (self.window):
+			self.window.connect("destroy", gtk.main_quit)
 		self.window.show_all()
 		
 	def btnGo_clicked(self, widget):
 	        url = self.wTree.get_widget("entry1").get_text()
 	        self.view.open(url)
 
-			
 if __name__ == "__main__":
 	hwg = HelloWorldGTK()
 	gtk.main()
